ecca613d7ea7c6e1eebead95571965633d117977
31shavy@gmail.com <31shavy@gmail.com>
2020-05-23 19:08:08 +0530

    session photo upload

--- api/main.py
+++ api/main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 
 @app.route('/video/upload', methods=['GET', 'POST'])
-def upload_file():
+def video_file():
     if request.method == 'POST':
         # request.headers['Access-Control-Allow-Origin'] = '*'
         f = request.files['file']
@@ -15,6 +15,19 @@ def upload_file():
         f.save("files/videos/"+secure_filename(f.filename))
         # response = jsonify({"order_id": 123, "status": "shipped"})
         # response.headers.add("Access-Control-Allow-Origin", "*")
+        response = make_response()
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        response.headers.add('Access-Control-Allow-Headers', "*")
+        response.headers.add('Access-Control-Allow-Methods', "*")
+        return response
+
+
+@app.route('/attend/session', methods=['GET', 'POST'])
+def attend_session():
+    if request.method == 'POST':
+        f = request.files['file']
+        # print(request.content)
+        f.save("files/attendance/"+secure_filename("test.png"))
         response = make_response()
         response.headers.add("Access-Control-Allow-Origin", "*")
         response.headers.add('Access-Control-Allow-Headers', "*")
